9c2114e2db21168372a97effb13fc44a1441e2ff
SergiuTripon <contact@sergiu-tripon.com>
2016-03-31 22:15:24 +0100

    Completed statistical analysis of most of the features.

---
+++ statistical_analysis.py
@@ -0,0 +1,35 @@
+
+# Following: http://blog.dato.com/beginners-guide-to-click-through-rate-prediction-with-logistic-regression
+
+import graphlab as gl
+data = gl.SFrame.read_csv('data_train.txt', delimiter='\t', verbose=False)
+
+# print(data.head(1))
+
+# print 'Overall CTR: {}'.format(data['click'].mean())
+
+'''
+print(data.groupby('hour',
+                   {'CTR': gl.aggregate.MEAN('click')}
+                   ).sort('CTR', ascending=False).print_rows(197, 2))
+'''
+
+'''
+print(data.groupby('click',
+                   {'FREQ': gl.aggregate.FREQ_COUNT('click')}
+                   ).print_rows(2, 2))
+'''
+
+print(data.groupby('key_page_url',
+                   {'CTR': gl.aggregate.MEAN('click')},
+                   {'FREQ': gl.aggregate.FREQ_COUNT('key_page_url')}
+                   ).sort('CTR', ascending=False).print_rows(2, 3))
+
+'''
+print(data.groupby('ad_slot_width',
+                       {'ad_slot_height': gl.aggregate.SELECT_ONE('ad_slot_height')},
+                   {'CTR': gl.aggregate.MEAN('click')},
+                   {'WIDTH FREQ': gl.aggregate.FREQ_COUNT('ad_slot_width')},
+                   {'HEIGHT FREQ': gl.aggregate.FREQ_COUNT('ad_slot_height')}
+                   ).sort('CTR', ascending=False).print_rows(11, 5))
+'''
